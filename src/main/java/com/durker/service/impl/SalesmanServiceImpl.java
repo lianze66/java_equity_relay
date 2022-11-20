6da9ced4925cf1ec622d438bb1266f4da2a30242
lianze66 <312239109@qq.com>
2022-11-20 10:11:09 +0800

    销售人员的二维码修改成了全路径。

--- src/main/java/com/durker/service/impl/SalesmanServiceImpl.java
+++ src/main/java/com/durker/service/impl/SalesmanServiceImpl.java
@@ -4,9 +4,31 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.durker.bean.Salesman;
 import com.durker.mapper.SalesmanMapper;
 import com.durker.service.ISalesmanService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.List;
 
 @Service
 public class SalesmanServiceImpl extends ServiceImpl<SalesmanMapper, Salesman> implements ISalesmanService {
 
+    @Value("${file.server}")
+    private String fileServer;
+
+    @Override
+    public List<Salesman> list() {
+        List<Salesman> list = super.list();
+        for (Salesman salesman : list) {
+            salesman.setQrCode(fileServer + salesman.getQrCode());
+        }
+        return list;
+    }
+
+    @Override
+    public Salesman getById(Serializable id) {
+        Salesman salesman = super.getById(id);
+        salesman.setQrCode(fileServer + salesman.getQrCode());
+        return salesman;
+    }
 }
